b832cf91453878611a186b310e568cfbcbecc790
MathMatowicki <matowicki@gmail.com>
2020-03-03 10:30:47 +0000

    Dodanie przykladowego programu oraz plikow zrodlowych.

--- plik.c
+++ plik.c
@@ -2,13 +2,28 @@
  
 int main(int argc, char *argv[])
 {
-    FILE *f = fopen("notatki.txt", "r");    // otwiera plik do odczytu (musi istniec)
-    if (f == NULL)
+
+    
+    FILE *z = fopen("zrodlo.txt", "r");    // otwiera plik do odczytu (musi istniec)
+    FILE *c = fopen("cel.txt","w");
+    if (z == NULL)
     {
         perror("Nie udalo sie otworzyc pliku notatki.txt");
         return 1;
     }
+
+    int test[5] = {1, 16, 256, 1024, 16384};
+    
+  
+    for (int i = 0; i < 5; i++)
+    {
+        fread(c,test[i],1*test[i]/* tu ma byc ile razy pobieramy bajty*/,z);
+        fwrite(z,test[i],1*test[i],c);
+
+    }
+    // while(fread("plik.txt"),,,)
     puts("Plik otwarty pomyslnie!");
-    fclose(f);
+    fclose(z);
+    fclose(c);
     return 0;
 }
